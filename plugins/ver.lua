19657ec8149427ff08dcd376726f36c9e7c99470
Tele-Fox <Tele-Fox@users.noreply.github.com>
2016-07-29 02:21:33 +0430

    Create ver.lua

---
+++ plugins/ver.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return [[انلاین بات ورژن 1
+ساخته شده توسط فاکس تیم
+
+با این ربات میتوانید در سرور خود با ربات بازی کنید
+]]
+end
+return {
+  description = "Shows bot ver", 
+  usage = "/help: Shows bot ver",
+  patterns = {
+    "^([/!]ver)$"
+  }, 
+  run = run 
+}
