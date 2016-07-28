dc2b42e744ed3bdc8554f407c188b7444936eac3
Tele-Fox <Tele-Fox@users.noreply.github.com>
2016-07-29 02:17:42 +0430

    Create gs.lua

---
+++ plugins/gs.lua
@@ -0,0 +1,16 @@
+do
+
+function run(msg, matches)
+  return [[بازی ها :
+CounterSource (درحال ساخت)
+Counter1.6 (در دسترس)
+Call of duty4 (در دسترس)]]
+end
+return {
+  description = "Shows bot gs", 
+  usage = "/help: Shows bot gs",
+  patterns = {
+    "^([/!]gs)$"
+  }, 
+  run = run 
+}
